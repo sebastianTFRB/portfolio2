07e71c03df0fc6ed56fca414bf3341afe769a3b8
sebastianTFRB <sebastian.fajardo@campusucc.edu.co>
2024-05-14 22:51:35 -0500

    commit

---
+++ src/app/components/Nav_bar.tsx
@@ -0,0 +1,32 @@
+
+import { FaLinkedin } from 'react-icons/fa'; //error por sitaxis (desaparece contenido)
+import { FaGithub } from 'react-icons/fa';
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
+
+
+
+
+
+
+
+const Navbar = () => {
+  return (
+    <nav className=" mb-20 flex itmes-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin/>
+        <FaGithub/>
+        <FaInstagram/>
+        <FaSquareXTwitter/>
+
+      </div>
+   
+    </nav>
+  )
+}
+
+export default Navbar;
